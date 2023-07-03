5e8c1031609e21d2e64fd4b3390a3572985cd3e5
patricio-chavez <patricio.chavez@realnaut.com>
2023-07-03 17:32:49 +0200

    limpia-iam.sh

--- devops/scripts/limpia-iam.sh
+++ devops/scripts/limpia-iam.sh
@@ -2,11 +2,36 @@
 
 # Verificar si el usuario ya existe
 NOMBRE_USUARIO="cloud_automation"
+
 aws iam get-user --user-name $NOMBRE_USUARIO >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "El usuario $NOMBRE_USUARIO ya existe en IAM."
 else
   echo "El usuario $NOMBRE_USUARIO no existe en IAM. No se realizará ninguna acción."
+  exit 1
+fi
+
+# Obtener las Access Key del usuario
+echo "Obteniendo todas las Access Key del usuario..."
+ACCESS_KEYS=$(aws iam list-access-keys --user-name $NOMBRE_USUARIO --query "AccessKeyMetadata[].AccessKeyId" --output text)
+
+# Verificar si existen Access Key para el usuario
+if [[ -z "$ACCESS_KEYS" ]]; then
+  echo "No se encontraron Access Key para el usuario $NOMBRE_USUARIO en IAM. No se realizará ninguna acción."
+  exit 1
+fi
+
+# Eliminar las Access Key del usuario
+echo "Eliminando las Access Key del usuario $NOMBRE_USUARIO en IAM..."
+for ACCESS_KEY in $ACCESS_KEYS; do
+  aws iam delete-access-key --user-name $NOMBRE_USUARIO --access-key-id $ACCESS_KEY
+done
+
+# Verificar si la eliminación fue exitosa
+if [ $? -eq 0 ]; then
+  echo "Access Key del usuario $NOMBRE_USUARIO eliminadas exitosamente en IAM."
+else
+  echo "Error al eliminar las Access Key del usuario $NOMBRE_USUARIO en IAM."
   exit 1
 fi
 
